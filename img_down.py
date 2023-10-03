a91e96e930c1ea63c19b186927cbe95f9050e062
lxp7313 <290462247@qq.com>
2023-10-04 00:19:23 +0800

    1

--- img_down.py
+++ img_down.py
@@ -4,40 +4,44 @@ import os
 
 from PIL import Image, ImageFilter, ImageDraw, ImageFont
 
+img_folder = "./images"
+# 获取文件夹中所有的图片文件路径
+img_files = [os.path.join(img_folder, f) for f in os.listdir(img_folder) if
+             f.endswith(".jpg") or f.endswith(".png")]
+for i in img_files:
+    print(i)
+    file_img = i.split('\\')[1]
+    print(file_img)
+    # 打开图片
+    image = Image.open(i)
+    # 调整图像大小并税化
+    resized_image = image.resize((image.width * 2, image.height * 2), Image.LANCZOS).filter(ImageFilter.SHARPEN)
+
+    # 设置空白高度
+    padding_top = 50
+
+    # 计算新图像的尺寸
+    new_width = image.width * 2
+    new_height = image.height * 2 + padding_top
+
+    # 创建新图像
+    new_image = Image.new("RGBA", (new_width, new_height), color=(255, 255, 255, 0))
+
+    # 将原始图像粘贴到新图像中
+    new_image.paste(resized_image, (0, padding_top))
+
+    # 添加文字
+    draw = ImageDraw.Draw(new_image)
+    text = "看图猜成语"
+    font = ImageFont.truetype("STXINWEI.TTF", 24)  # 字体和字号
+    text_width, text_height = draw.textsize(text, font)
+    text_x = (new_width - text_width) // 2  # 文字水平居中
+    text_y = (padding_top - text_height) // 2  # 文字垂直居中
+    draw.text((text_x, text_y), text, font=font, fill=(255, 0, 0))  # 红色文字
 
 
-
-
-# 打开图片
-image = Image.open("./images/唉声叹气.png")
-# 调整图像大小并税化
-resized_image = image.resize((image.width * 2, image.height * 2), Image.LANCZOS).filter(ImageFilter.SHARPEN)
-
-# 设置空白高度
-padding_top = 100
-
-# 计算新图像的尺寸
-new_width = image.width * 2
-new_height = image.height * 2 + padding_top
-
-# 创建新图像
-new_image = Image.new("RGB", (new_width, new_height), color=(255, 255, 255))
-
-# 将原始图像粘贴到新图像中
-new_image.paste(image, (0, padding_top))
-
-# 添加文字
-draw = ImageDraw.Draw(new_image)
-text = "这是一行红色文字"
-font = ImageFont.truetype("arial.ttf", 24)  # 字体和字号
-text_width, text_height = font.getsize(text)
-text_x = (new_width - text_width) // 2  # 文字水平居中
-text_y = (padding_top - text_height) // 2  # 文字垂直居中
-draw.text((text_x, text_y), text, font=font, fill=(255, 0, 0))  # 红色文字
-
-
-# 保存处理后的图像
-new_image.save("./images/sharp_唉声叹气.png")
+    # 保存处理后的图像
+    new_image.save("./images/new/" + file_img)
 
 
 
